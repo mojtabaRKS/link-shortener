5ae00a8a6ac8abdbc9862e23071de8f535464fd7
Mojtaba Rakhisi <m.rakhisi@liateam.net>
2022-03-13 22:06:04 +0330

    refac: remove useless function

--- src/helpers.php
+++ src/helpers.php
@@ -21,15 +21,3 @@ if (!function_exists('newRoute')) {
         ], $requirements, $defaults, '', $schemes, [$verb]);
     }
 }
-
-if (! function_exists('generateRandomString')) {
-    function generateRandomString($length = 10) {
-        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $length; $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
-    }
-}
