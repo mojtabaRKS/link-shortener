8a3a299e7eae3b3f95eb8c32b6c1c26c31925e23
Mojtaba Rakhisi <m.rakhisi@liateam.net>
2022-03-14 19:31:42 +0330

    feat: add crud routes

--- src/routes.php
+++ src/routes.php
@@ -5,23 +5,43 @@ use Symfony\Component\Routing\RouteCollection;
 
 $collection = new RouteCollection();
 
-$collection->add('links.show', newRoute(
-    'GET', 
-    '/links/{id}', 
-    'App\Controllers\LinkController@show', 
-    ['id' => '[0-9]+']
+$collection->add('links.index', newRoute(
+    'DELETE',
+    '/links',
+    'App\Controllers\LinkController@index',
 ));
 
 $collection->add('links.store', newRoute(
-    'POST', 
-    '/links', 
-    'App\Controllers\LinkController@store', 
+    'POST',
+    '/links',
+    'App\Controllers\LinkController@store',
+));
+
+$collection->add('links.show', newRoute(
+    'GET',
+    '/links/{id}',
+    'App\Controllers\LinkController@show',
+    ['id' => '[0-9]+']
+));
+
+$collection->add('links.update', newRoute(
+    'PUT',
+    '/links/{id}',
+    'App\Controllers\LinkController@update',
+    ['id' => '[0-9]+']
+));
+
+$collection->add('links.delete', newRoute(
+    'DELETE',
+    '/links/{id}',
+    'App\Controllers\LinkController@delete',
+    ['id' => '[0-9]+']
 ));
 
 $collection->add('links.redirect', newRoute(
-    'GET', 
-    '/{code}', 
-    'App\Controllers\LinkController@redirect', 
+    'GET',
+    '/{code}',
+    'App\Controllers\LinkController@redirect',
 ));
 
 return $collection;
