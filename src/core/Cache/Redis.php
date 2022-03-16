0a0985b13d24d2c322f653067a21dc7db3015077
mojtaba rakhisi <mojtaba.rakhisi@gamil.com>
2022-03-16 03:52:40 +0330

    feat (cache) : add redis driver to app

---
+++ src/core/Cache/Redis.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Core\Cache;
+
+use Closure;
+use Core\Redis\RedisConnection;
+
+class Redis
+{
+    private $redis;
+
+    public function __construct()
+    {
+        $this->redis = RedisConnection::getInstance()->getConnection();
+    }
+
+    public static function __callStatic($name, $arguments)
+    {
+        dd($name, $arguments);
+        return (new self)->$name(...$arguments);
+    }
+
+    public function set(string $key, $value, int $ttl = 0)
+    {
+        $this->redis->set($key, $value);
+
+        if ($ttl > 0) {
+            $this->redis->expire($key, $ttl);
+        }
+    }
+
+    public function get(string $key)
+    {
+        return $this->redis->get($key);
+    }
+
+    public function delete(string $key)
+    {
+        $this->redis->del($key);
+    }
+
+    public function flush()
+    {
+        $this->redis->flushAll();
+    }
+
+    public function remember(string $key, Closure $value, int $ttl = 0)
+    {
+        if ($this->redis->exists($key)) {
+            return $this->get($key);
+        }
+
+        $this->set($key, call_user_func($value), $ttl);
+
+        return $value;
+    }
+
+    public function exists(string $key)
+    {
+        return $this->redis->exists($key);
+    }
+}
---
+++ src/core/Cache/RedisConnection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Core\Redis;
+
+use Predis\Client;
+
+class RedisConnection
+{
+    // Hold the class instance.
+    private static $instance = null;
+    private $conn;
+
+    // The db connection is established in the private constructor.
+    private function __construct()
+    {
+        $this->conn = new Client(array(
+            "scheme" => $_ENV['REDIS_SCHEME'],
+            "host" => $_ENV['REDIS_HOST'],
+            "port" => $_ENV['REDIS_PORT'],
+            "password" => $_ENV['REDIS_PASSWORD']
+        ));
+    }
+
+    /**
+     * Cloning and unserialization are not permitted for singletons.
+     */
+    protected function __clone() { }
+
+    public function __wakeup()
+    {
+        throw new \Exception("Cannot unserialize singleton");
+    }
+
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public function getConnection()
+    {
+        return $this->conn;
+    }
+}
